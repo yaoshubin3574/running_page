4f4f9728bba755285f1f7b81ee11cd8f25d8ba33
yaoshubin3574 <33371737+yaoshubin3574@users.noreply.github.com>
2025-05-10 11:51:30 +0800

    Update strava_sync.py

--- run_page/strava_sync.py
+++ run_page/strava_sync.py
@@ -6,7 +6,7 @@ from generator import Generator
 
 
 # for only run type, we use the same logic as garmin_sync
-def run_strava_sync(client_id, client_secret, refresh_token, only_run=False):
+def run_strava_sync(client_id, client_secret, refresh_token, only_run=True):
     generator = Generator(SQL_FILE)
     generator.set_strava_config(client_id, client_secret, refresh_token)
     # if you want to refresh data change False to True
