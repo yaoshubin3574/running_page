17256ce15d365fbd155c72b49eff0fb7adf364f2
yaoshubin3574 <yaoshubin3574@gmail.com>
2025-05-10 11:44:42 +0800

    0510

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,38 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const getBasePath = () => {
+  const baseUrl = import.meta.env.BASE_URL;
+  return baseUrl === '/' ? '' : baseUrl;
+};
+
+const data: ISiteMetadataResult = {
+  siteTitle: '風が強く吹いている',
+  siteUrl: 'http://localhost:5173/',
+  logo: 'https://p.sda1.dev/23/dabbc3fe25a87a2af69e4cd2f90d1490/logo.jpg',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Summary',
+      url: `${getBasePath()}/summary`,
+    },
+    {
+      name: 'Blog',
+      url: 'https://github.com/yihong0618/gitblog',
+    },
+    {
+      name: 'About',
+      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+    },
+  ],
+};
+
+export default data;
